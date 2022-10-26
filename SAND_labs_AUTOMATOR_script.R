eff2114f45d858b94f74070d60883b614c9e5a3d
jacobtnyoung <39632608+jacobtnyoung@users.noreply.github.com>
2022-10-26 13:51:52 -0700

    Major changes to file names and paths

---
+++ SAND_labs_AUTOMATOR_script.R
@@ -0,0 +1,22 @@
+# ------------------------------ #
+# Automator Script for SAND Labs #
+# ------------------------------ #
+
+# Load the packages needed.
+require( knitr )
+require( rmarkdown )
+require( here )
+
+# Creates the html output.
+render( here:: here( "/_labs/lab-01-intro-r.Rmd", "html_document" ) )
+
+!!!WORKING ON GETTING THIS TO RUN
+
+!!!NEED THE OTHER LABS
+
+# Close this script.
+quit( save = "no" )
+
+# ------------- #
+# END OF SCRIPT #
+# ------------- #
