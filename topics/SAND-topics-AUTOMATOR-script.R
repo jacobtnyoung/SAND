014af8db12e810659e3caab5cd0a1ecc2950dbb8
jacobyoung20 <39632608+jacobtnyoung@users.noreply.github.com>
2022-11-09 09:32:21 -0700

    ran automator script

--- topics/SAND-topics-AUTOMATOR-script.R
+++ topics/SAND-topics-AUTOMATOR-script.R
@@ -1,14 +1,25 @@
+# -------------------- #
+# Automate topic pages #
+
+# This script knits the pages for the SAND website
+
+
+# Load the needed libraries
 require( knitr )
 require( rmarkdown )
+require( here )
 
-# Creates the html output.
-render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/RWorkshop/index.Rmd", "html_document" )
-render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/RWorkshop/R_Basics.Rmd", "html_document" )
-render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/RWorkshop/R_Working_with_Data.Rmd", "html_document" )
-render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/RWorkshop/R_Programming.Rmd", "html_document" )
-render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/RWorkshop/R_Data_Visualization.Rmd", "html_document" )
-render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/RWorkshop/R_Data_Example.Rmd", "html_document" )
-render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/RWorkshop/R_Markdown.Rmd", "html_document" )
 
-# Close this script.
+# Creates the html output
+render( here::here( "topics/page-01-intro.Rmd" ), "html_document" )
+render( here::here( "topics/page-02-network-data.Rmd" ), "html_document" )
+render( here::here( "topics/page-03-r-network-vis.Rmd" ), "html_document" )
+render( here::here( "topics/page-04-centrality.Rmd" ), "html_document" )
+render( here::here( "topics/page-05-subgroups.Rmd" ), "html_document" )
+render( here::here( "topics/page-06-bipartite.Rmd" ), "html_document" )
+render( here::here( "topics/page-07-ergm.Rmd" ), "html_document" )
+render( here::here( "topics/page-08-sabm.Rmd" ), "html_document" )
+
+
+# Close this script
 quit( save = "no" )
