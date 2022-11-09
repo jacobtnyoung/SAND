0b14915a192bd1d399e76b057d3f78b7b72f0d43
jacobyoung20 <39632608+jacobtnyoung@users.noreply.github.com>
2022-11-09 10:16:33 -0700

    updates to labs and automator

---
+++ labs/SAND-labs-AUTOMATOR-script.R
@@ -0,0 +1,29 @@
+# -------------------- #
+# Automate topic pages #
+
+# This script knits the pages for the SAND website
+
+
+# Load the needed libraries
+require( knitr )
+require( rmarkdown )
+require( here )
+
+
+# Creates the html output
+render( here::here( "labs/lab-01-r-intro.Rmd" ), "html_document" )
+render( here::here( "labs/lab-02-nets-intro.Rmd" ), "html_document" )
+render( here::here( "labs/lab-03-visualization-intro.Rmd" ), "html_document" )
+render( here::here( "labs/lab-04-degree-centrality.Rmd" ), "html_document" )
+render( here::here( "labs/lab-05-close-between-centrality.Rmd" ), "html_document" )
+render( here::here( "labs/lab-06-subgroups.Rmd" ), "html_document" )
+render( here::here( "labs/lab-07-bipartite.Rmd" ), "html_document" )
+render( here::here( "labs/lab-08-projection-weighted.Rmd" ), "html_document" )
+render( here::here( "labs/lab-09-ergm-intro.Rmd" ), "html_document" )
+render( here::here( "labs/lab-10-ergm-gof.Rmd" ), "html_document" )
+render( here::here( "labs/lab-11-sabm-intro.Rmd" ), "html_document" )
+render( here::here( "labs/lab-12-sabm-coevolution.Rmd" ), "html_document" )
+
+
+# Close this script
+quit( save = "no" )
