b5cb7c1c42b308e72c875016b9979182610c399e
jacobtnyoung <CRJ605@asu.edu>
2024-01-04 11:13:13 -0700

    edits to lecture 1 and to code for that lecture

--- lecture_syntax/lecture-syntax-01-introduction.R
+++ lecture_syntax/lecture-syntax-01-introduction.R
@@ -41,6 +41,53 @@ gplot(example.net, usearrows = TRUE, usecurve = TRUE, main="",coord=coords,verte
 gplot(example.net, usearrows = TRUE, usecurve = TRUE, main="",coord=coords,vertex.col="lightblue",edge.col = rep(c("orange","green","red"),21),arrowhead.cex=0.25)
 
 
+
+# to create image for intro lecture
+
+library( sna )
+
+actors  <- 21
+ModNet <- matrix( 0, nrow = actors, ncol = actors )
+rownames( ModNet ) <- 1:actors
+colnames( ModNet ) <- 1:actors
+
+ModNet[ c( 1,2,3 ), c( 1,2,3 ) ] <- 1
+ModNet[ c( 4,5,6 ), c( 4,5,6 ) ] <- 1
+ModNet[ c( 7,8,9 ), c( 7,8,9 ) ] <- 1
+ModNet[ c( 10,11,12 ), c( 10,11,12 ) ] <- 1
+ModNet[ c( 13,14,15 ), c( 13,14,15 ) ] <- 1
+ModNet[ c( 16,17,18 ), c( 16,17,18 ) ] <- 1
+ModNet[ c( 19,20,21 ), c( 19,20,21 ) ] <- 1
+
+gplot( ModNet, gmode = "graph", vertex.col = "#1ac461" )
+
+
+# make some connections
+
+ConNet <- ModNet
+
+ConNet[ 3, 7 ] <- 1
+ConNet[ 8, 15 ] <- 1
+
+gplot( ConNet, gmode = "graph", vertex.col = "#1ac461" )
+
+# make more connections
+ConNet[ 12, 10 ] <- 1
+ConNet[ 3, 6 ] <- 1
+ConNet[ 13, 21 ] <- 1
+ConNet[ 9, 18 ] <- 1
+ConNet[ 21, 16 ] <- 1
+ConNet[ 12, 4 ] <- 1
+
+gplot( ConNet, gmode = "graph", vertex.col = "#1ac461" )
+
+
+
+
+
+
+
+
 # ============ # 
 # End of syntax.
 # ============ # 
