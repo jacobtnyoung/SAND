0e8151db8a9b69d8f9cfc8a835572d6d126018a4
jacobtnyoung <CRJ605@asu.edu>
2024-01-23 09:25:34 -0700

    edits for week 3

--- hmwk/SAND-hmwk-AUTOMATOR-script.R
+++ hmwk/SAND-hmwk-AUTOMATOR-script.R
@@ -6,8 +6,14 @@ require( knitr )
 require( rmarkdown )
 require( here )
 
-# Creates the html output
+# Create the instructions
 render( here::here( "hmwk/hmwk-01-instrutions.Rmd" ), "html_document" )
+render( here::here( "hmwk/hmwk-02-instrutions.Rmd" ), "html_document" )
+
+# Create the templates
+render( here::here( "hmwk/hmwk-02-template.Rmd" ), "html_document" )
+
+# Create the solutions
 
 
 # Close this script
