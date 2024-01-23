8cc8f35b1fd5c720a92b4ad5f6dfbb86c9cb06ee
jacobtnyoung <CRJ605@asu.edu>
2024-01-23 09:34:15 -0700

    minor edits to hyperlinks for homework #2

--- labs/lab-BUILD-ALL.R
+++ labs/lab-BUILD-ALL.R
@@ -12,17 +12,19 @@ require( here )
 
 # Creates the html output
 render( here::here( "labs/lab-01-r-intro.Rmd" ), "html_document" )
-render( here::here( "labs/lab-02-nets-intro.Rmd" ), "html_document" )
-render( here::here( "labs/lab-03-visualization-intro.Rmd" ), "html_document" )
-render( here::here( "labs/lab-04-degree-centrality.Rmd" ), "html_document" )
-render( here::here( "labs/lab-05-close-between-centrality.Rmd" ), "html_document" )
-render( here::here( "labs/lab-06-subgroups.Rmd" ), "html_document" )
-render( here::here( "labs/lab-07-bipartite.Rmd" ), "html_document" )
-render( here::here( "labs/lab-08-projection-weighted.Rmd" ), "html_document" )
-render( here::here( "labs/lab-09-ergm-intro.Rmd" ), "html_document" )
-render( here::here( "labs/lab-10-ergm-gof.Rmd" ), "html_document" )
-render( here::here( "labs/lab-11-sabm-intro.Rmd" ), "html_document" )
-render( here::here( "labs/lab-12-sabm-coevolution.Rmd" ), "html_document" )
+render( here::here( "labs/lab-02-rmarkdown.Rmd" ), "html_document" )
+render( here::here( "labs/lab-03-network-data.Rmd" ), "html_document" )
+
+###HERE WITH UPDATING
+#render( here::here( "labs/lab-04-degree-centrality.Rmd" ), "html_document" )
+#render( here::here( "labs/lab-05-close-between-centrality.Rmd" ), "html_document" )
+#render( here::here( "labs/lab-06-subgroups.Rmd" ), "html_document" )
+#render( here::here( "labs/lab-07-bipartite.Rmd" ), "html_document" )
+#render( here::here( "labs/lab-08-projection-weighted.Rmd" ), "html_document" )
+#render( here::here( "labs/lab-09-ergm-intro.Rmd" ), "html_document" )
+#render( here::here( "labs/lab-10-ergm-gof.Rmd" ), "html_document" )
+#render( here::here( "labs/lab-11-sabm-intro.Rmd" ), "html_document" )
+#render( here::here( "labs/lab-12-sabm-coevolution.Rmd" ), "html_document" )
 
 
 # Close this script
